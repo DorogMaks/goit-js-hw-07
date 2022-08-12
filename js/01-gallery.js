c49b1f9e5df44a8adcee31758e4481a7f8705cde
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-08-12 16:06:02 +0300

    add task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", createGalleryList(galleryItems));
+
+function createGalleryList(imagesArray) {
+  return imagesArray
+    .map((elem) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${elem.preview}"
+            data-source="${elem.original}"
+            alt="${elem.description}"
+          />
+        </a>
+    </div>`;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.getAttribute(
+      "data-source"
+    )}" width="800" height="600">
+`);
+
+  instance.show();
+
+  gallery.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") instance.close();
+  });
+}
